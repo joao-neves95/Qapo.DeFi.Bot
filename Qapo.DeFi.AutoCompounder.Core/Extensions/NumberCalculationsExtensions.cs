70476906708cdea74b433f21172ba26f033b8ec6
João Neves <joao95neves@gmail.com>
2021-12-14 17:29:10 +0000

    [.Core.Extensions/]: Added NumberCalculationsExtensions utils class

---
+++ Qapo.DeFi.AutoCompounder.Core/Extensions/NumberCalculationsExtensions.cs
@@ -0,0 +1,11 @@
+
+namespace Qapo.DeFi.AutoCompounder.Core.Extensions
+{
+    public static class NumberCalculationsExtensions
+    {
+        public static int IncreasePercentage(this int @num, float percentage)
+        {
+            return @num;
+        }
+    }
+}
