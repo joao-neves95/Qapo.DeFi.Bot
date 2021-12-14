8e46831e089cdc5d2a3fe0485fcf5d22e0d936a3
João Neves <joao95neves@gmail.com>
2021-12-14 17:28:44 +0000

    [.Core.Extensions/]: Added ObjectExtensions utils

---
+++ Qapo.DeFi.AutoCompounder.Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Qapo.DeFi.AutoCompounder.Core.Extensions
+{
+    public static class ObjectExtensions
+    {
+        #nullable enable
+
+        public static T? ThrowIfNull<T>(this T? @instance, string paramName) where T : class
+        {
+            return @instance ?? throw new ArgumentNullException(paramName, "Instance of type '" + typeof(T).Name + "' is null");
+        }
+
+        public static T ThrowIfDefault<T>(this T @instance, string paramName) where T : class
+        {
+            if (@instance.Equals(default(T)))
+            {
+                throw new ArgumentException("Instance of type '" + typeof(T).Name + "' is default", paramName);
+            }
+
+            return @instance;
+        }
+
+        public static T? ThrowIfNullOrDefault<T>(this T? @instance, string paramName) where T : class
+        {
+            return @instance.ThrowIfNull(paramName).ThrowIfDefault(paramName);
+        }
+
+        #nullable disable
+    }
+}
