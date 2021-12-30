e9c623e3911b9c43bf96469a343b7ec40dd1eeb1
João Neves <joao95neves@gmail.com>
2021-12-30 00:18:04 +0000

    Merge remote-tracking branch 'origin/master'

--- Qapo.DeFi.AutoCompounder.Worker/Worker.cs
+++ Qapo.DeFi.AutoCompounder.Worker/Worker.cs
@@ -84,26 +84,47 @@ namespace Qapo.DeFi.AutoCompounder.Worker
 
         private async Task HandleDbFileStoresUpdate(AppConfig appConfig)
         {
-            await this._mediator.Send(
-                new UpdateLocalDbFromDataFiles()
-                {
-                    AppConfig = appConfig
-                }
-            );
+            try
+            {
+                await this._mediator.Send(
+                    new UpdateLocalDbFromDataFiles()
+                    {
+                        AppConfig = appConfig
+                    }
+                );
+            }
+            catch (Exception e)
+            {
+                this._logger.LogError(
+                    e,
+                    $"ERROR - [{nameof(Worker)}.{nameof(HandleDbFileStoresUpdate)}], while updating DBs"
+                );
+            }
         }
 
         private async Task HandleVaultsExecution(AppConfig appConfig)
         {
             List<LockedVault> lockedVaults = await this._lockedVaultsStore.GetAll();
 
-            for (int i = 0; i < lockedVaults.Count; ++i) {
-                await this._mediator.Send(
-                    new AutoCompoundStrategy()
-                    {
-                        AppConfig = appConfig,
-                        LockedVault = lockedVaults[i]
-                    }
-                );
+            for (int i = 0; i < lockedVaults.Count; ++i)
+            {
+                try
+                {
+                    await this._mediator.Send(
+                        new AutoCompoundStrategy()
+                        {
+                            AppConfig = appConfig,
+                            LockedVault = lockedVaults[i]
+                        }
+                    );
+                }
+                catch (Exception e)
+                {
+                    this._logger.LogError(
+                        e,
+                        $"ERROR - [{nameof(Worker)}.{nameof(HandleVaultsExecution)}], while strategy execution"
+                    );
+                }
             }
         }
 
