9472f5fc0f19d29a8f2641dd587fb4003c569071
João Neves <joao95neves@gmail.com>
2022-01-03 20:09:35 +0000

    [.Core.Models.Config]: Removed unneeded props

--- Qapo.DeFi.AutoCompounder.Core/Models/Config/BlockchainsConfig.cs
+++ Qapo.DeFi.AutoCompounder.Core/Models/Config/BlockchainsConfig.cs
@@ -6,25 +6,13 @@ namespace Qapo.DeFi.AutoCompounder.Core.Models.Config
 {
     public class BlockchainsConfig
     {
-        public string PolygonRpcEndpoint { get; set; }
-
-        public int PolygonChainId { get; set; }
-
         public float PolygonDefaultGas { get; set; }
 
         public float PolygonMaxGas { get; set; }
 
-        public string FantomRpcEndpoint { get; set; }
-
-        public int FantomChainId { get; set; }
-
         public float FantomDefaultGas { get; set; }
 
         public float FantomMaxGas { get; set; }
-
-        public string BscRpcEndpoint { get; set; }
-
-        public int BscChainId { get; set; }
 
         public float BscDefaultGas { get; set; }
 
